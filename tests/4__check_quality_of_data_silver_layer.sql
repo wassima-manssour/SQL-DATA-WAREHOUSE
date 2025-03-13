17630c9fb71b52c88a979688af3319df4e86aeac
WASSIMA MANSSOUR <112273899+wassima-manssour@users.noreply.github.com>
2025-03-13 15:06:42 +0000

    Update 4__check_quality_of_data_silver_layer.sql

--- tests/4__check_quality_of_data_silver_layer.sql
+++ tests/4__check_quality_of_data_silver_layer.sql
@@ -1,6 +1,24 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs various quality checks for data consistency, accuracy, 
+    and standardization across the 'silver' layer. It includes checks for:
+    - Null or duplicate primary keys.
+    - Unwanted spaces in string fields.
+    - Data standardization and consistency.
+    - Invalid date ranges and orders.
+    - Data consistency between related fields.
+
+Usage Notes:
+    - Run these checks after data loading Silver Layer.
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
+
+
 USE DataWarehouse
-
-
 /*==========================================================
         silver.crm_cust_info TABLE checks on SILVER layer
 ===========================================================*/
